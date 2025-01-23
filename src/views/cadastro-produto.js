cba41d79a347d8dd60b983d1275eade677466cf2
gabrazones <gabrielferfernandes@outlook.com>
2025-01-23 08:42:36 -0300

    att cadastro produtod

--- src/views/cadastro-produto.js
+++ src/views/cadastro-produto.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { data, useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import Stack from '@mui/material/Stack';
 
@@ -7,16 +7,12 @@ import Card from '../components/card';
 import FormGroup from '../components/form-group';
 
 import { mensagemSucesso, mensagemErro } from '../components/toastr';
-
-// import '../custom.css';
 import axios from 'axios';
 import { BASE_URL } from '../config/axios';
 
 function CadastroProduto() {
   const { idParam } = useParams();
-
   const navigate = useNavigate();
-
   const baseURL = `${BASE_URL}/Produto`;
 
   const [id, setId] = useState('');
@@ -37,50 +33,8 @@ function CadastroProduto() {
   const [idFornecedor, setIdFornecedor] = useState('');
   const [idSetor, setIdSetor] = useState('');
 
-  const [dados, setDados] = useState([]);
-
-  function inicializar() {
-    if (idParam == null) {
-      setId('');
-      setNome('');
-      setPrecoVenda('');
-      setQuantidade('');
-      setDescricao('');
-      setQuantMin('');
-      setQuantMax('');
-      setLote('');
-      setVencimento('');
-      setCodigo('');
-      setPerecibilidade(false);
-      setDataEntrada('');
-      setUniMedida('');
-      setPrecoCompra('');
-      setCodBarras('');
-      setIdFornecedor('');
-      setIdSetor('');
-    } else {
-      setId(dados.id);
-      setNome(dados.nome);
-      setPrecoVenda(dados.precoVenda);
-      setQuantidade(dados.quantidade);
-      setDescricao(dados.descricao);
-      setQuantMin(dados.quantMin);
-      setQuantMax(dados.quantMax);
-      setLote(dados.lote);
-      setVencimento(dados.vencimento);
-      setCodigo(dados.codigo);
-      setPerecibilidade(dados.perecibilidade);
-      setDataEntrada(dados.dataEntrada);
-      setUniMedida(dados.uniMedida);
-      setPrecoCompra(dados.precoCompra);
-      setCodBarras(dados.codBarras);
-      setIdFornecedor(dados.idFornecedor);
-      setIdSetor(dados.idSetor);
-    }
-  }
-
   async function salvar() {
-    let data = {
+    const data = {
       id,
       nome,
       precoVenda,
@@ -99,103 +53,223 @@ function CadastroProduto() {
       idFornecedor,
       idSetor,
     };
-    data = JSON.stringify(data);
-    if (idParam == null) {
-      await axios
-        .post(baseURL, data, {
-          headers: { 'Content-Type': 'application/json' },
-        })
-        .then((response) => {
-          mensagemSucesso(`Produto ${nome} cadastrado com sucesso!`);
-          navigate(`/listagem-produtos`);
-        })
-        .catch((error) => {
-          mensagemErro(error.response.data);
-        });
-    } else {
-      await axios
-        .put(`${baseURL}/${idParam}`, data, {
-          headers: { 'Content-Type': 'application/json' },
-        })
-        .then((response) => {
-          mensagemSucesso(`Produto ${nome} alterado com sucesso!`);
-          navigate(`/listagem-produtos`);
-        })
-        .catch((error) => {
-          mensagemErro(error.response.data);
-        });
+
+    try {
+      if (!idParam) {
+        await axios.post(baseURL, data);
+        mensagemSucesso(`Produto "${nome}" cadastrado com sucesso!`);
+      } else {
+        await axios.put(`${baseURL}/${idParam}`, data);
+        mensagemSucesso(`Produto "${nome}" atualizado com sucesso!`);
+      }
+      navigate('/listagem-produtos');
+    } catch (error) {
+      mensagemErro(error.response?.data || 'Erro ao salvar o produto.');
     }
   }
 
-  async function buscar() {
+  async function buscarProduto() {
     if (idParam) {
-      await axios.get(`${baseURL}/${idParam}`).then((response) => {
-        setDados(response.data);
-      });
-      setId(dados.id);
-      setNome(dados.nome);
-      setPrecoVenda(dados.precoVenda);
-      setQuantidade(dados.quantidade);
-      setDescricao(dados.descricao);
-      setQuantMin(dados.quantMin);
-      setQuantMax(dados.quantMax);
-      setLote(dados.lote);
-      setVencimento(dados.vencimento);
-      setCodigo(dados.codigo);
-      setPerecibilidade(dados.perecibilidade);
-      setDataEntrada(dados.dataEntrada);
-      setUniMedida(dados.uniMedida);
-      setPrecoCompra(dados.precoCompra);
-      setCodBarras(dados.codBarras);
-      setIdFornecedor(dados.idFornecedor);
-      setIdSetor(dados.idSetor);
+      try {
+        const response = await axios.get(`${baseURL}/${idParam}`);
+        const produto = response.data;
+        setId(produto.id);
+        setNome(produto.nome);
+        setPrecoVenda(produto.precoVenda);
+        setQuantidade(produto.quantidade);
+        setDescricao(produto.descricao);
+        setQuantMin(produto.quantMin);
+        setQuantMax(produto.quantMax);
+        setLote(produto.lote);
+        setVencimento(produto.vencimento);
+        setCodigo(produto.codigo);
+        setPerecibilidade(produto.perecibilidade);
+        setDataEntrada(produto.dataEntrada);
+        setUniMedida(produto.uniMedida);
+        setPrecoCompra(produto.precoCompra);
+        setCodBarras(produto.codBarras);
+        setIdFornecedor(produto.idFornecedor);
+        setIdSetor(produto.idSetor);
+      } catch (error) {
+        mensagemErro('Erro ao buscar o produto.');
+      }
     }
   }
 
   useEffect(() => {
-    buscar(); // eslint-disable-next-line
-  }, [id]);
-
-  if (!dados) return null;
+    buscarProduto();
+  }, [idParam]);
 
   return (
     <div className='container'>
-      <Card title='Cadastro de Produtos'>
+      <Card title='Cadastro de Produto'>
         <div className='row'>
           <div className='col-lg-12'>
             <div className='bs-component'>
-              <FormGroup label='Nome: *' htmlFor='inputNome'>
+              <FormGroup label='Nome:' htmlFor='inputNome'>
                 <input
                   type='text'
                   id='inputNome'
                   value={nome}
                   className='form-control'
-                  name='nome'
                   onChange={(e) => setNome(e.target.value)}
                 />
               </FormGroup>
-              <FormGroup label='Preço de Venda: *' htmlFor='inputPrecoVenda'>
+
+              <FormGroup label='Preço de Venda:' htmlFor='inputPrecoVenda'>
                 <input
                   type='number'
                   id='inputPrecoVenda'
                   value={precoVenda}
                   className='form-control'
-                  name='precoVenda'
                   onChange={(e) => setPrecoVenda(e.target.value)}
                 />
               </FormGroup>
-              {/* Adicione os outros campos similares, como quantidade, descrição, lote, etc. */}
-              <Stack spacing={1} padding={1} direction='row'>
-                <button
-                  onClick={salvar}
-                  type='button'
-                  className='btn btn-success'
-                >
+
+              <FormGroup label='Quantidade:' htmlFor='inputQuantidade'>
+                <input
+                  type='number'
+                  id='inputQuantidade'
+                  value={quantidade}
+                  className='form-control'
+                  onChange={(e) => setQuantidade(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup label='Descrição:' htmlFor='inputDescricao'>
+                <textarea
+                  id='inputDescricao'
+                  value={descricao}
+                  className='form-control'
+                  onChange={(e) => setDescricao(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup label='Quantidade Mínima:' htmlFor='inputQuantMin'>
+                <input
+                  type='number'
+                  id='inputQuantMin'
+                  value={quantMin}
+                  className='form-control'
+                  onChange={(e) => setQuantMin(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup label='Quantidade Máxima:' htmlFor='inputQuantMax'>
+                <input
+                  type='number'
+                  id='inputQuantMax'
+                  value={quantMax}
+                  className='form-control'
+                  onChange={(e) => setQuantMax(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup label='Lote:' htmlFor='inputLote'>
+                <input
+                  type='text'
+                  id='inputLote'
+                  value={lote}
+                  className='form-control'
+                  onChange={(e) => setLote(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup label='Data de Vencimento:' htmlFor='inputVencimento'>
+                <input
+                  type='date'
+                  id='inputVencimento'
+                  value={vencimento}
+                  className='form-control'
+                  onChange={(e) => setVencimento(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup label='Código:' htmlFor='inputCodigo'>
+                <input
+                  type='text'
+                  id='inputCodigo'
+                  value={codigo}
+                  className='form-control'
+                  onChange={(e) => setCodigo(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup label='Perecibilidade:' htmlFor='inputPerecibilidade'>
+                <input
+                  type='checkbox'
+                  id='inputPerecibilidade'
+                  checked={perecibilidade}
+                  onChange={(e) => setPerecibilidade(e.target.checked)}
+                />
+              </FormGroup>
+
+              <FormGroup label='Data de Entrada:' htmlFor='inputDataEntrada'>
+                <input
+                  type='date'
+                  id='inputDataEntrada'
+                  value={dataEntrada}
+                  className='form-control'
+                  onChange={(e) => setDataEntrada(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup label='Unidade de Medida:' htmlFor='inputUniMedida'>
+                <input
+                  type='text'
+                  id='inputUniMedida'
+                  value={uniMedida}
+                  className='form-control'
+                  onChange={(e) => setUniMedida(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup label='Preço de Compra:' htmlFor='inputPrecoCompra'>
+                <input
+                  type='number'
+                  id='inputPrecoCompra'
+                  value={precoCompra}
+                  className='form-control'
+                  onChange={(e) => setPrecoCompra(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup label='Código de Barras:' htmlFor='inputCodBarras'>
+                <input
+                  type='text'
+                  id='inputCodBarras'
+                  value={codBarras}
+                  className='form-control'
+                  onChange={(e) => setCodBarras(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup label='ID do Fornecedor:' htmlFor='inputIdFornecedor'>
+                <input
+                  type='number'
+                  id='inputIdFornecedor'
+                  value={idFornecedor}
+                  className='form-control'
+                  onChange={(e) => setIdFornecedor(e.target.value)}
+                />
+              </FormGroup>
+
+              <FormGroup label='ID do Setor:' htmlFor='inputIdSetor'>
+                <input
+                  type='number'
+                  id='inputIdSetor'
+                  value={idSetor}
+                  className='form-control'
+                  onChange={(e) => setIdSetor(e.target.value)}
+                />
+              </FormGroup>
+
+              <Stack spacing={2} direction='row'>
+                <button onClick={salvar} className='btn btn-success'>
                   Salvar
                 </button>
                 <button
-                  onClick={inicializar}
-                  type='button'
+                  onClick={() => navigate('/listagem-produtos')}
                   className='btn btn-danger'
                 >
                   Cancelar
