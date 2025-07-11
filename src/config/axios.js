4160dd69c89a7905d0f2791a79456dc3c37e4872
gabrazones <gabrielferfernandes@outlook.com>
2025-07-11 19:32:46 -0300

    FEAT: conectando front com API backend

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,3 +1,1 @@
-export const BASE_URL = 'https://my-json-server.typicode.com/GabrielFFernandes/jsonfake'
-export const BASE_URL2 = 'https://my-json-server.typicode.com/LucasBavuso/jsonfake'
-export const BASE_URL3 = 'https://my-json-server.typicode.com/p-mendoncad/jsonfake'
+export const BASE_URL = 'http://localhost:8080/api/v1';
